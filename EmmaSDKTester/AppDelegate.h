adb4f183d88749e3f90721ec68321640e254e67e
dolphin su <dolphinsu@pixnet.tw>
2015-11-18 17:45:08 +0800

    init commit

---
+++ EmmaSDKTester/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmmaSDKTester
+//
+//  Created by Dolphin Su on 11/18/15.
+//  Copyright © 2015 PIXNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmmaSDKTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmmaSDKTester
+//
+//  Created by Dolphin Su on 11/18/15.
+//  Copyright © 2015 PIXNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
